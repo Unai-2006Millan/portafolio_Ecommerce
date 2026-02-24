03805b4425d5e113706ef91909f97a0fd8e1a527
Unai-2006Millan <unairafales@gmail.com>
2026-02-24 12:15:16 +0100

    Cambios puntuales

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const { Client } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const client = new Client({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'unai-2006Nerea',
-    database: 'E-commerce'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 client.connect()
